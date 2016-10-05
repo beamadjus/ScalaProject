19da4fcc285b182a610822a4883386f33c9bb8b2
coleenbartido <bartido.coleen@gmail.com>
2016-10-05 18:56:26 +0800

    Add files via upload
    Updated the file "Project.scala".

--- ScalaProject/src/Project.scala
+++ ScalaProject/src/Project.scala
@@ -4,24 +4,26 @@ object Project {
 
   def main(args: Array[String]) {
 
-    val src = Source.fromFile("C:\\Users\\Bea Madjus\\Desktop\\test1.txt")
+    var words = scala.io.Source.fromFile("book.txt","ISO-8859-1")
 
-    for (line <- src .getLines()
+      .getLines
       .flatMap(_.split("\\W+"))
-      .filter(x => x.matches("[A-Za-z]+") )) {
+      .filter(x => x.matches("[A-Za-z]+") && x.length>1)
+      .foldLeft(Map.empty[String, Int]){
 
-     println(line)
+        (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
 
     }
-  }
 
-  def countWords(text: String) = {
-    val counts = scala.collection.mutable.Map.empty[String, Int].withDefaultValue(0)
-    for (rawWord <- text.split("[ ,!.]+")) {
-      val word = rawWord.toLowerCase;
-      counts(word) += 1;
+    val writer = new PrintWriter(new File("wordCount.txt"))
+
+    val sort = ListMap(words.toSeq.sortBy(_._1.toLowerCase):_*)
+
+    for ((k,v) <- sort) {
+      val string = "%s - %s".format(k,v)
+      writer.write(string + "\n")
     }
-    counts;
-
+    writer.close()
   }
+
 }
